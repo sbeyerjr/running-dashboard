7fd21f9876f48fba9d4a4f0decfcb8b53e019e33
Steven Beyer <sbeyerjr@gmail.com>
2019-05-18 14:42:33 -0700

    initial init

---
+++ main.js
@@ -0,0 +1,41 @@
+var options = {
+  chart: {
+    height: 150,
+    type: "area"
+  },
+  series: [
+    {
+      name: "Average BPM",
+      data: [60, 64, 65, 78, 83, 90, 92, 94, 90, 84, 76, 65]
+    }
+  ],
+  fill: {
+    type: "gradient",
+    gradient: {
+      shadeIntensity: 1,
+      opacityFrom: 0.7,
+      opacityTo: 0.9,
+      stops: [0, 90, 100]
+    }
+  },
+  xaxis: {
+    categories: [
+      "00:00",
+      "03:23",
+      "05:56",
+      "9:45",
+      "12:12",
+      "15:02",
+      "17:38",
+      "23:26",
+      "25:15",
+      "30:04",
+      "31:23",
+      "34:12"
+    ]
+  }
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+chart.render();
